8f13e010e297813c844d3ce083ee74cb93c3e934
Rosita311 <rosita-rampertaap@kpnmail.nl>
2021-01-09 17:01:07 +0100

    add function compareGuesses

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 function generateTarget() {
-   return Math.floor(Math.random() * 9) 
-}
-
+   return Math.floor(Math.random() * 10) 
+};
 
 console.log(generateTarget());
+
+function compareGuesses(human, computer, number) {
+
+  if((number-human) <= (number-computer)) {
+       return true;
+      } else {
+       return false;
+  } };
+
+console.log(compareGuesses());
+
